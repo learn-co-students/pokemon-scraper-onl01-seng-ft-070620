430b28d355256a63f04187e795f47ef93a047d61
Doug Schallmoser <dougschallmoser@mgail.com>
2020-08-03 22:20:55 -0700

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -16,7 +16,7 @@ class Pokemon
         SQL
 
         db.execute(sql, name, type)
-        @id = db.execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
+        # @id = db.execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
     end 
 
     def self.find(id, db)
@@ -26,6 +26,9 @@ class Pokemon
         WHERE id = ? 
         LIMIT 1
         SQL
+
+        # pokemon = db.execute(sql, id).flatten
+        # self.new(id: pokemon[0], name: pokemon[1], type: pokemon[2], db: db)
 
         db.execute(sql, id).collect do |row|
             self.new(id: row[0], name: row[1], type: row[2], db: db)
