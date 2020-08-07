08e139cce92a7a4884b9092034243ba140901847
Amanda_Katherine <walker.akw@gmail.com>
2020-08-07 01:07:23 -0400

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,36 @@
+require 'pry'
 class Pokemon
+
+    attr_accessor :name, :type, :db, :id
+    # attr_reader :id
+
+    def initialize (attributes)
+        attributes.each{|key, value| self.send(("#{key}="),value)}
+    end
+
+    def self.save(name, type, db) 
+        @db = db
+            sql = <<-SQL 
+                INSERT INTO pokemon (name, type)
+                VALUES (?, ?)
+            SQL
+
+        @db.execute(sql, name, type)
+    end
+
+    def self.new_from_db(row)
+        attributes = {:id => row[0], :name => row[1], :type => row[2]}
+        new_pokemon = self.new(attributes)
+    end
+
+    def self.find(id, db)
+        @db = db
+        sql = <<-SQL
+            SELECT *
+            FROM pokemon
+            WHERE id = ?
+        SQL
+
+        @db.execute(sql, id).map {|row| Pokemon.new_from_db(row)}.first
+    end
 end
